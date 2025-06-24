e7ac66fd5293a7007a0c80d40b2af7ef770f8e3c
Miwacer <mixanavvy@gmail.com>
2025-06-24 14:56:31 +0300

    Feat: Add Crew API

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from airport.models import Crew
+from airport.serializers import (
+    CrewSerializer,
+)
+
+
+class CrewViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
