67a4fa512fcca4ac872cd94594237f746e015057
Miwacer <mixanavvy@gmail.com>
2025-07-04 12:13:07 +0300

    feat: Add filter by route & date for Flight

--- airport/views.py
+++ airport/views.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
+from .filters import filter_by_date
+
+from django.db.models import Q
+
 from rest_framework import viewsets, mixins
 
 from airport.models import Crew, Flight, Route, Airport, Airplane, AirplaneType, Order
-
 from airport.serializers import (
     FlightListSerializer,
     RouteSerializer,
@@ -33,6 +38,32 @@ class FlightViewSet(
     mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
 ):
     queryset = Flight.objects.all()
+
+    @staticmethod
+    def _params_to_ints(qs):
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self, departure_time=None):
+        departure_time = self.request.query_params.get("departure_time")
+        arrival_time = self.request.query_params.get("arrival_time")
+        route = self.request.query_params.get("route")
+
+        queryset = self.queryset
+
+        if route:
+            queryset = queryset.filter(
+                Q(route__source__name__icontains=route) |
+                Q(route__destination__name__icontains=route)
+            )
+
+        if departure_time:
+            queryset = filter_by_date(queryset, departure_time, "departure_time")
+
+        if arrival_time:
+            queryset = filter_by_date(queryset, arrival_time, "arrival_time")
+
+        return queryset
+
 
     def get_serializer_class(self):
         if self.action == "list":
